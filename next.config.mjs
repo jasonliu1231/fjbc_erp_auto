ce424eadf224dc19c353acec505e222add428c55
jasonliu1231 <liuzaiyao1231@gmail.com>
2025-02-12 15:34:29 +0800

    環境變數

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
 const nextConfig = {
   env: {
     // 個人 API
-    NEXT_PUBLIC_API_URL_8102: "http://172.16.161.118:8102/test_api",
+    // NEXT_PUBLIC_API_URL_8102: "http://172.16.161.118:8102/test_api",
     // NEXT_PUBLIC_API_URL_8102: "http://192.168.68.100:8102/test_api",
 
     // 測試 API
     NEXT_PUBLIC_API_URL_8100: "http://172.16.150.31:8100/test_api",
     NEXT_PUBLIC_API_URL_8101: "http://172.16.150.31:8101/test_api",
-    // NEXT_PUBLIC_API_URL_8102: "http://172.16.150.31:8102/test_api",
+    NEXT_PUBLIC_API_URL_8102: "http://172.16.150.31:8102/test_api",
 
     // 正式 API
     NEXT_PUBLIC_API_URL_VIDEO: "http://172.16.150.39:8200"
